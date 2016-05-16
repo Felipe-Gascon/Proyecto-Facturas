7303337c981d8b8ed6426a36a541ba850a3ca8ae
Felipe <felipegascon279@gmail.com>
2016-05-16 11:15:50 +0200

    Signed-off-by: Felipe <felipegascon279@gmail.com>

--- Facturas/src/vMaquinaria.java
+++ Facturas/src/vMaquinaria.java
@@ -9,6 +9,8 @@ import javax.swing.JScrollPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -17,11 +19,13 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 
 @SuppressWarnings("serial")
@@ -35,8 +39,16 @@ public class vMaquinaria extends JFrame {
 	JScrollPane scrollPane;
 	Maquinaria ma = new Maquinaria();
 	ConexionDB facturas;
+	@SuppressWarnings("unused")
 	private static String Trabajos = "Trabajos_Maq";
+	@SuppressWarnings("unused")
 	private static String Coste = "Precios";
+	private JTextField textNom;
+	private JTextField textSup;
+	private JLabel lblAo;
+	private JTextField txtYear;
+	private final JLabel lblNewLabel = new JLabel("Total");
+	private JTextField txtTot;
 
 
 
@@ -65,7 +77,7 @@ public class vMaquinaria extends JFrame {
 		datos= new JTable(dtm);
 
 		scrollPane = new JScrollPane(datos);
-		scrollPane.setBounds(5, 60, 419, 156);
+		scrollPane.setBounds(5, 85, 419, 131);
 		contentPane.add(scrollPane);
 		contentPane.add(btnAadirDatos);
 
@@ -76,6 +88,40 @@ public class vMaquinaria extends JFrame {
 		JButton btnModificar = new JButton("Modificar");
 		btnModificar.setBounds(316, 227, 108, 23);
 		contentPane.add(btnModificar);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(5, 11, 47, 23);
+		contentPane.add(lblNombre);
+		
+		textNom = new JTextField();
+		textNom.setBounds(60, 12, 86, 20);
+		contentPane.add(textNom);
+		textNom.setColumns(10);
+		
+		JLabel lblSuperficie = new JLabel("Superficie");
+		lblSuperficie.setBounds(207, 11, 78, 23);
+		contentPane.add(lblSuperficie);
+		
+		textSup = new JTextField();
+		textSup.setBounds(286, 12, 86, 20);
+		contentPane.add(textSup);
+		textSup.setColumns(10);
+		
+		lblAo = new JLabel("A\u00F1o");
+		lblAo.setBounds(5, 45, 32, 23);
+		contentPane.add(lblAo);
+		
+		txtYear = new JTextField();
+		txtYear.setBounds(60, 45, 86, 20);
+		contentPane.add(txtYear);
+		txtYear.setColumns(10);
+		lblNewLabel.setBounds(217, 45, 47, 20);
+		contentPane.add(lblNewLabel);
+		
+		txtTot = new JTextField();
+		txtTot.setBounds(286, 43, 86, 20);
+		contentPane.add(txtTot);
+		txtTot.setColumns(10);
 
 		for(int column=0;column<Columnas.length;column++)
 		{
@@ -83,8 +129,25 @@ public class vMaquinaria extends JFrame {
 			this.dtm.addColumn(Columnas[column]);
 		}
 		muestraDatos(dtm);
+		
+		datos.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent Mouse_evt)
+			{
+				JTable tabla = (JTable) Mouse_evt.getSource();
+				Point point = Mouse_evt.getPoint();
+				int row = tabla.rowAtPoint(point);
+				if(Mouse_evt.getClickCount()==2)
+				{
+					String valor;
+					valor =  (String) datos.getValueAt(datos.getSelectedRow(), 1);
+					System.out.println(valor);
+				}
+			}
+		});
 
 	}
+	
+
 
 	public void muestraDatos(DefaultTableModel miDTM)
 	{
@@ -119,6 +182,4 @@ public class vMaquinaria extends JFrame {
 			}
 		}
 	}
-
-
 }
